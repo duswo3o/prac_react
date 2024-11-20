280ac5e2940c8c8437adb529bd4ce2cfe2947491
yeonjae <pyj3740@gmail.com>
2024-11-20 11:37:22 +0900

    feat: cleanup
    useEffect의 첫 번째 argument인 effect에서
    effect 함수의 return을 사용하여 cleanup 될 때 동작 실행

--- my-first-react-app/src/App.js
+++ my-first-react-app/src/App.js
@@ -1,34 +1,40 @@
 import React, { useState, useEffect } from "react";
 
-function App() {
-  const [counter, setValue] = useState(0);
-  const [keyword, setKeyword] = useState("");
+function Hello() {
+  // first way
+  function byeFn() {
+    console.log("bye :(");
+  }
+  function hiFn() {
+    console.log("created :)");
+    return byeFn;
+  }
+  useEffect(hiFn, []);
 
-  const onClick = () => setValue((prev) => prev + 1);
-  const onChange = (event) => setKeyword(event.target.value);
-
+  // second way
   useEffect(() => {
-    console.log("i run Only Once");
+    console.log("hi :)");
+    return () => console.log("bye :(");
   }, []);
-  useEffect(() => {
-    console.log("i run when 'keyword' changes");
-  }, [keyword]);
-  useEffect(() => {
-    console.log("i run when 'counter' changes");
-  }, [counter]);
-  useEffect(() => {
-    console.log("i run when keyword&counter changes");
-  }, [keyword, counter]);
+
+  // third way
+  useEffect(function () {
+    console.log("hi :)");
+    return function () {
+      console.log("bye :(");
+    };
+  }, []);
+
+  return <h1>Hello</h1>;
+}
+
+function App() {
+  const [showing, setShowing] = useState(false);
+  const onClick = () => setShowing((prev) => !prev);
   return (
     <div>
-      <input
-        value={keyword}
-        onChange={onChange}
-        type="text"
-        placeholder="Search Here..."
-      />
-      <h1>{counter}</h1>
-      <button onClick={onClick}>Click me</button>
+      {showing ? <Hello /> : null}
+      <button onClick={onClick}>{showing ? "Hide" : "Show"}</button>
     </div>
   );
 }
