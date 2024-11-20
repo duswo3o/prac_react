9b3338661b9c839da19d433043a46f29ef5cdb59
yeonjae <pyj3740@gmail.com>
2024-11-20 10:46:42 +0900

    feat: Deps
    코드의 특정한 부분만 변화했을 때, 원하는 코드를 실행

--- my-first-react-app/src/App.js
+++ my-first-react-app/src/App.js
@@ -2,14 +2,31 @@ import React, { useState, useEffect } from "react";
 
 function App() {
   const [counter, setValue] = useState(0);
+  const [keyword, setKeyword] = useState("");
+
   const onClick = () => setValue((prev) => prev + 1);
-  console.log("i run all the time");
-  const iRunOnlyOnce = () => {
+  const onChange = (event) => setKeyword(event.target.value);
+
+  useEffect(() => {
     console.log("i run Only Once");
-  };
-  useEffect(iRunOnlyOnce, []);
+  }, []);
+  useEffect(() => {
+    console.log("i run when 'keyword' changes");
+  }, [keyword]);
+  useEffect(() => {
+    console.log("i run when 'counter' changes");
+  }, [counter]);
+  useEffect(() => {
+    console.log("i run when keyword&counter changes");
+  }, [keyword, counter]);
   return (
     <div>
+      <input
+        value={keyword}
+        onChange={onChange}
+        type="text"
+        placeholder="Search Here..."
+      />
       <h1>{counter}</h1>
       <button onClick={onClick}>Click me</button>
     </div>
